3a2a736c0eb4e57ab6ec326ad9692c412c268479
Kaushalya13 <nimanthikaushalya13@gmail.com>
2023-07-10 18:58:40 +0530

    Updating

--- src/main/java/com/example/chatapplication/controller/RegisterFromController.java
+++ src/main/java/com/example/chatapplication/controller/RegisterFromController.java
@@ -4,16 +4,35 @@ import com.example.chatapplication.model.clientModel;
 import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Properties;
 import java.util.ResourceBundle;
 
 public class RegisterFromController implements Initializable {
+    private final static String URL = "jdbc:mysql://localhost:3306/chat_application";
+    private final static Properties props = new Properties();
+
+    static {
+        props.setProperty("user","root");
+        props.setProperty("password","1234");
+    }
     @FXML
     private AnchorPane root;
 
@@ -28,10 +47,31 @@ public class RegisterFromController implements Initializable {
 
     @FXML
     private PasswordField txtPassword;
+    @FXML
+    private ImageView imgBack;
 
 
-    public void btnRegisterOnAction(ActionEvent actionEvent) {
+    public void btnRegisterOnAction(ActionEvent actionEvent) throws SQLException {
+        String id = txtUser_id.getText();
+        String name = txtUserName.getText();
+        String password = txtPassword.getText();
 
+        try (Connection connection = DriverManager.getConnection(URL, props)){
+            String sql = "INSERT INTO user "+" VALUES(?,?,?)";
+
+            PreparedStatement pstm = connection.prepareStatement(sql);
+            pstm.setString(1,id);
+            pstm.setString(2,name);
+            pstm.setString(3,password);
+
+            int add = pstm.executeUpdate();
+
+            if (add > 0){
+                new Alert(Alert.AlertType.CONFIRMATION,"User Added", ButtonType.OK).show();
+            }else {
+                new Alert(Alert.AlertType.WARNING,"Try again", ButtonType.OK).show();
+            }
+        }
     }
 
     @Override
@@ -46,5 +86,16 @@ public class RegisterFromController implements Initializable {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public void backOnAction(MouseEvent mouseEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/login_from.fxml"));
+        AnchorPane anchorPane = loader.load();
+        Scene scene = new Scene(anchorPane);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.setTitle("Login From");
+        stage.show();
+        root.getScene().getWindow().hide();
     }
 }
